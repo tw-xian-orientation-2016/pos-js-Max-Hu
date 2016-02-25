78932daeda36d80924afbb81f7d91416cfa9275c
Max-Hu <huzhiwise@gmail.com>
2016-02-25 11:44:24 +0800

    implement count number task

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,1 +1,17 @@
 //TODO: Please write code in this file.
+function countNumber(code) {
+  var map = {};
+  for (var i = 0; i < code.length; i++) {
+    var array = code[i].split("-")
+    if (array.length === 2) {
+      map[array[0]] = parseInt(array[1]);
+    }else {
+      if (map[array[0]] != undefined) {
+        map[array[0]] += 1;
+      }else {
+        map[array[0]] = 1;
+      }
+    }
+  }
+  return map;
+}
