477de20b9d3369a0096d6e2a4be801964eb830b6
Max-Hu <huzhiwise@gmail.com>
2016-02-25 11:45:04 +0800

    create count number task test

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -17,22 +17,30 @@ describe('pos', function() {
     ];
   });
 
-  it('should print correct text', function() {
+  // it('should print correct text', function() {
+  //
+  //   spyOn(console, 'log');
+  //
+  //   printReceipt(inputs);
+  //
+  //   var expectText =
+  //     '***<没钱赚商店>收据***\n' +
+  //     '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+  //     '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+  //     '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+  //     '----------------------\n' +
+  //     '总计：51.00(元)\n' +
+  //     '节省：7.50(元)\n' +
+  //     '**********************';
+  //
+  //   expect(console.log).toHaveBeenCalledWith(expectText);
+  // });
 
-    spyOn(console, 'log');
+  it('should get number correctly', function() {
 
-    printReceipt(inputs);
-
-    var expectText =
-      '***<没钱赚商店>收据***\n' +
-      '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-      '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-      '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
-      '----------------------\n' +
-      '总计：51.00(元)\n' +
-      '节省：7.50(元)\n' +
-      '**********************';
-
-    expect(console.log).toHaveBeenCalledWith(expectText);
+    var items = countNumber(inputs);
+    expect(items["ITEM000001"]).toEqual(5);
+    expect(items["ITEM000003"]).toEqual(2);
+    expect(items["ITEM000005"]).toEqual(3);
   });
 });
